48afa765e522fb90bb601b3d85d7d5aaf6c7e3fd
Ben Lin <lbhan1221@gmail.com>
2022-05-21 23:53:02 -0400

    updated information for 3B

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
         <img src={dp} alt="Me"/>
       </div>
       <h1>Hi, my name is Ben Lin.</h1>
-      <p>3A Honours Computer Science (Co-op) @ University of Waterloo</p>
+      <p>3B Honours Computer Science (Co-op) @ University of Waterloo</p>
     </div>
   );
 }
