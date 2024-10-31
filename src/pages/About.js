7c4844909181b669b3a0221fb5f85784ffb93054
Ben Lin <lbhan1221@gmail.com>
2024-10-31 00:17:19 -0400

    changed photos page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,11 +4,10 @@ function About() {
   return (
     <div className='about'>
       <h1>About Me</h1>
-      <p>I am a Computer Science <s>student</s> New Grad from the University of Waterloo!</p>
-      <p>I've recently completed my Honours Bachelor of Computer Science with a minor in Combinatorics and Optimization.</p>
+      <p>I'm a Honours Bachelor of Computer Science graduate with a minor in Combinatorics and Optimization.</p>
       <p>Some of my current interests include machine learning, artificial intelligence, computer vision and computer graphics.</p>
       <p>I have worked previously as a Backend Software Engineer at Shakudo, a Full Stack Developer at the Royal Canadian Air Force, a Software Engineer Intern at Nuro technologies Inc., and as a Compiler Software Engineer at Huawei Canada</p>
-      <p>Some of my hobbies include dancing, drawing, filmmaking, drumming, and playing all kinds of sports!</p>
+      <p>Some of my hobbies include dancing, drawing, and filmmaking.</p>
     </div>
   );
 }
