0c840971355a5277aec5a80b0bfea3e320ad2674
Ben Lin <lbhan1221@gmail.com>
2025-01-09 17:20:02 -0500

    added education and socials links

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,38 @@ function App() {
       <div className='main-container'>
         <SideNav/>
         <div class='content'>
-          <h1>Hi, I'm Ben.</h1>
           <section id='bio'>
             <h2>BIO</h2>
             <p>
               I am a Bachelor's of Computer Science Graduate from the University of Waterloo, minoring in Combinatorics and Optimization.
               My interests include Computer Vision, Machine Learning, Artificial Intelligence, Graphics, and Game Theory.
               I have interned previously at Huawei, Nuro, the Royal Canadian Air Force, and Shakudo.
-              Apart from schooling and my professional career, I am a dancer, previously a part of <a href='https://www.instagram.com/uw.origins/'>UW Origins</a> and president of <a href='https://www.instagram.com/uwhiphop/'>UW Hip Hop</a>
+              Apart from schooling and my professional career, I am a dancer, previously a part of <a href='https://www.instagram.com/uw.origins/'>UW Origins</a> and president of <a href='https://www.instagram.com/uwhiphop/'>UW Hip Hop</a>.
             </p>
+          </section>
+          <section id='education'>
+            <h2>Education</h2>
+            <p>
+              I attended the University of Waterloo from 2019-2024 and graduated with an Honours Bachelor of Computer Science with a minor in Combinatorics and Optimization.
+              Here's a summary of what I did there:
+            </p>
+            <h3>Final GPA: 89.38/100</h3>
+            <h3>Awards: Dean's Honours List, University of Waterloo President's Scholarship of Distinction</h3>
+            <h3>Relevant Coursework:</h3>
+            <ul className='double-column-list'>
+              <li>Machine Learning</li>
+              <li>Artificial Intelligence</li>
+              <li>Computational Vision</li>
+              <li>Computer Networks</li>
+              <li>Cryptography</li>
+              <li>Game Theory</li>
+              <li>Stochastic Processes</li>
+              <li>Algorithms and Data Structures</li>
+              <li>User Interfaces</li>
+              <li>Numerical Computation</li>
+              <li>Optimization</li>
+              <li>Network Flow Theory</li>
+            </ul>
           </section>
         </div>
       </div>
--- src/ui/sidenav.jsx
+++ src/ui/sidenav.jsx
@@ -1,15 +1,19 @@
-import './sidenav.css'
+import './sidenav.css';
 import SideNavLink from './sidenavlink';
+import github from '../assets/github-logo-black.png';
+import linkedin from '../assets/linkedin-logo-black.png';
 
 export default function SideNav() {
   return (
     <div className="sidenav">
-      Ben Lin
+      <h2>Ben Lin</h2>
       <SideNavLink linktext='bio'>Bio</SideNavLink>
       <SideNavLink linktext='education'>Education</SideNavLink>
       <SideNavLink linktext='work-experience'>Work Experience</SideNavLink>
       <SideNavLink linktext='projects'>Projects</SideNavLink>
       <SideNavLink linktext='misc'>Misc</SideNavLink>
+      <a href='https://github.com/benlin1221'><img src={github} width="50"/></a>
+      <a href='https://www.linkedin.com/in/benlin1221/'><img src={linkedin} width="50"/></a>
     </div>
   );
 }
